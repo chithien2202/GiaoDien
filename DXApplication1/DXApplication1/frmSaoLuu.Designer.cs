f9be917f47bba9bae4e2ac7ba328c7854826fde1
DESKTOP-6V52PV4\chith <chithien2202@gmail.com>
2017-04-26 13:40:06 +0700

    hoàn thành frmSaoLuu

--- DXApplication1/DXApplication1/frmSaoLuu.Designer.cs
+++ DXApplication1/DXApplication1/frmSaoLuu.Designer.cs
@@ -33,17 +33,17 @@
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
-            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnBackup = new DevExpress.XtraEditors.SimpleButton();
+            this.txtLocation = new DevExpress.XtraEditors.TextEdit();
+            this.btnBrower = new DevExpress.XtraEditors.SimpleButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLocation.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 59);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(64, 64);
             this.pictureBox1.TabIndex = 0;
@@ -52,7 +52,7 @@
             // labelControl1
             // 
             this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelControl1.Location = new System.Drawing.Point(84, 84);
+            this.labelControl1.Location = new System.Drawing.Point(88, 89);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(82, 18);
             this.labelControl1.TabIndex = 1;
@@ -73,56 +73,58 @@
             this.simpleButton2.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.simpleButton2.Appearance.Options.UseFont = true;
             this.simpleButton2.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.Image")));
-            this.simpleButton2.Location = new System.Drawing.Point(368, 136);
+            this.simpleButton2.Location = new System.Drawing.Point(361, 131);
             this.simpleButton2.Name = "simpleButton2";
             this.simpleButton2.Size = new System.Drawing.Size(109, 38);
             this.simpleButton2.TabIndex = 8;
             this.simpleButton2.Text = "&Hủy";
             // 
-            // simpleButton1
+            // btnBackup
             // 
-            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.simpleButton1.Appearance.Options.UseFont = true;
-            this.simpleButton1.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.Image")));
-            this.simpleButton1.Location = new System.Drawing.Point(224, 136);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(105, 38);
-            this.simpleButton1.TabIndex = 7;
-            this.simpleButton1.Text = "Đồng ý";
+            this.btnBackup.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackup.Appearance.Options.UseFont = true;
+            this.btnBackup.Image = ((System.Drawing.Image)(resources.GetObject("btnBackup.Image")));
+            this.btnBackup.Location = new System.Drawing.Point(217, 131);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(105, 38);
+            this.btnBackup.TabIndex = 7;
+            this.btnBackup.Text = "Sao lưu";
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
             // 
-            // textEdit1
+            // txtLocation
             // 
-            this.textEdit1.Location = new System.Drawing.Point(172, 82);
-            this.textEdit1.Name = "textEdit1";
-            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textEdit1.Properties.Appearance.Options.UseFont = true;
-            this.textEdit1.Size = new System.Drawing.Size(252, 22);
-            this.textEdit1.TabIndex = 9;
+            this.txtLocation.Location = new System.Drawing.Point(176, 87);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLocation.Properties.Appearance.Options.UseFont = true;
+            this.txtLocation.Size = new System.Drawing.Size(252, 22);
+            this.txtLocation.TabIndex = 9;
             // 
-            // simpleButton3
+            // btnBrower
             // 
-            this.simpleButton3.Location = new System.Drawing.Point(430, 81);
-            this.simpleButton3.Name = "simpleButton3";
-            this.simpleButton3.Size = new System.Drawing.Size(36, 23);
-            this.simpleButton3.TabIndex = 10;
-            this.simpleButton3.Text = "...";
+            this.btnBrower.Location = new System.Drawing.Point(434, 86);
+            this.btnBrower.Name = "btnBrower";
+            this.btnBrower.Size = new System.Drawing.Size(36, 23);
+            this.btnBrower.TabIndex = 10;
+            this.btnBrower.Text = "...";
+            this.btnBrower.Click += new System.EventHandler(this.btnBrower_Click);
             // 
             // frmSaoLuu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 180);
-            this.Controls.Add(this.simpleButton3);
-            this.Controls.Add(this.textEdit1);
+            this.ClientSize = new System.Drawing.Size(485, 194);
+            this.Controls.Add(this.btnBrower);
+            this.Controls.Add(this.txtLocation);
             this.Controls.Add(this.simpleButton2);
-            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.btnBackup);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "frmSaoLuu";
             this.Text = "Sao lưu dữ liệu";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLocation.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,8 +136,8 @@
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.LabelControl labelControl2;
         private DevExpress.XtraEditors.SimpleButton simpleButton2;
-        private DevExpress.XtraEditors.SimpleButton simpleButton1;
-        private DevExpress.XtraEditors.TextEdit textEdit1;
-        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton btnBackup;
+        private DevExpress.XtraEditors.TextEdit txtLocation;
+        private DevExpress.XtraEditors.SimpleButton btnBrower;
     }
 }
--- DXApplication1/DXApplication1/frmSaoLuu.cs
+++ DXApplication1/DXApplication1/frmSaoLuu.cs
@@ -8,14 +8,66 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using System.Data.SqlClient;
 
 namespace DXApplication1
 {
     public partial class frmSaoLuu : DevExpress.XtraEditors.XtraForm
     {
+        
         public frmSaoLuu()
         {
             InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-6V52PV4\\SQLEXPRESS;Initial Catalog=QLTB;Persist Security Info=True;User ID=sa;Password=sa2014");
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            string database = con.Database.ToString();
+            try
+            {
+                if (txtLocation.Text == string.Empty)
+                {
+                    XtraMessageBox.Show("Hãy chọn thư mục lưu", "Thông báo", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    string cmd = "BACKUP DATABASE [" + database + "] TO DISK='" + txtLocation.Text + "\\" + database + "_" + DateTime.Now.ToString("dd_MM_yy_HH_mm_ss") + ".bak'";
+
+                    SqlCommand command = new SqlCommand(cmd, con);
+
+                    if (con.State != ConnectionState.Open)
+                    {
+                        con.Open();
+                    }
+
+                    command.ExecuteNonQuery();
+                    con.Close();
+                    XtraMessageBox.Show("Sao lưu thành công", "Thông báo", MessageBoxButtons.OK);
+                    this.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+        }
+
+        private void btnBrower_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtLocation.Text = dlg.SelectedPath;
+                btnBackup.Enabled = true;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
